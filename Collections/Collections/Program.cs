acdd95cd24c822b7d2f826c7b1d31b9517cd35a3
unknown <matthewharre@gmail.com>
2022-06-29 12:10:08 -0700

    added more examples

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare arraylist - both undefined amount / defined
+            //ArrayList myArrayList = new ArrayList();
+            //ArrayList myArrayList2 = new ArrayList(100);
+
+            //myArrayList.Add(25);
+            //myArrayList.Add("Hello");
+            //myArrayList.Add(13.36);
+            //myArrayList.Add(13.36);
+
+            //myArrayList.Remove(13.36);
+
+            //myArrayList.RemoveAt(0);
+
+            //Console.WriteLine(myArrayList.Count);
+
+            //double sum = 0;
+
+            //foreach (object obj in myArrayList)
+            //{
+            //    if (obj is int)
+            //    {
+            //        sum += Convert.ToDouble(obj);
+            //    } else if (obj is double)
+            //    {
+            //        sum += (double)obj;
+            //    } else if(obj is string)
+            //    {
+            //        Console.WriteLine(obj);
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+
+        }
+    }
+}
---
+++ Hashtables/Hashtables/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+
+namespace Hashtables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable table = new Hashtable();
+            Student stud1 = new Student(1, "Bob", 30);
+            Student stud2 = new Student(2, "Jason", 45);
+
+            table.Add(stud1.id, stud1);
+            table.Add(stud2.id, stud2);
+
+            Student storedStudent1 = (Student)table[stud1.id];
+
+            //foreach (DictionaryEntry entry in table)
+            //{
+            //    Student temp = (Student)entry.Value;
+            //    Console.WriteLine($"{temp.id}");
+            //    Console.WriteLine($"{temp.name}");
+            //    Console.WriteLine($"{temp.GPA}");
+            //    Console.WriteLine("--------------");
+            //}
+
+            //simplier for each loop for hash tables
+            foreach (Student value in table.Values)
+            {
+                Console.WriteLine($"{value.id}");
+                Console.WriteLine($"{value.name}");
+                Console.WriteLine($"{value.GPA}");
+                Console.WriteLine("--------------");
+
+            }
+        }
+
+        class Student
+        {
+            //property called Id
+            public int id { get; set; }
+            //property called name
+            public string name { get; set; }
+
+            public float GPA { get; set; }
+            
+            // constructor
+            public Student(int id, string name, float GPA)
+            {
+                this.id = id;
+                this.name = name;
+                this. GPA = GPA;
+            }
+        }
+    }
+}
